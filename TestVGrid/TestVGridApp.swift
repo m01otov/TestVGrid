9ad2fe2afa1e9a4be50ac2e3df1035bb6391e8c5
m01otov <67689912+m01otov@users.noreply.github.com>
2021-12-08 19:57:04 +0500

    Initial Commit

---
+++ TestVGrid/TestVGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestVGridApp.swift
+//  TestVGrid
+//
+//  Created by Ilya Melnikov on 08.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct TestVGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
